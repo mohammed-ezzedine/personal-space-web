d51b0a38c01d8e4e88a8b05fa146b255256ee2dc
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2023-12-30 16:00:46 +0200

    Fix rendering of images

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    // serverBaseUrl: 'https://mohammed.ezzedine.me/api',
     serverBaseUrl: 'http://localhost:8080',
     auth: {
         issuerUrl: 'https://lemur-1.cloud-iam.com/auth/realms/mezzedine-auth-server',
